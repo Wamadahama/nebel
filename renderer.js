864cd5df58b678a91df67324cfd1bdd211c7c9f5
wamadahama <smurftown2@gmail.com>
2017-04-06 23:27:38 -0400

    left and right

--- renderer.js
+++ renderer.js
@@ -8,7 +8,8 @@ const {ipcRenderer} = require('electron')
 
 let constants = require('./constants')
 
-let imageBox = document.querySelector("#image")
+let imageBox   = document.querySelector("#image")
+let imageCount = document.querySelector("#count")
 
 let fileList = []
 let currentImage = 0
@@ -19,36 +20,44 @@ ipcRenderer.on('image-send', function(event, files) {
 
     /* Load the first file */
     imageBox.src = files[0];
+    imageCount.innerText = `${ currentImage+1 } \\ ${ fileList.length }`
 
 });
 
 // Handle left and right arrow keys
 document.onkeydown = (e) => {
+
   // Switch the image
   switch (e.keyCode) {
     case constants.keys.rightKey:
 
-      // Keep track of what image we are on
-      if (currentImage >= fileList.length) {
-        currentImage = 0
+      if(fileList.length != 1) {
+	currentImage += 1
       }
-
-      currentImage += 1
-
-      imageBox.src = fileList[currentImage]
 
       break;
     case constants.keys.leftKey:
 
-      // Keep track of what image we are on
-      if (currentImage >= fileList.length) {
-        currentImage = 0
+      if(currentImage == 0) {
+	  console.log(currentImage)
+	    currentImage = fileList.length-1
+      } else {
+
+	  if(fileList.length != 1) {
+	    currentImage -= 1;
+	  }
+
       }
-
-      currentImage -= 1
-
-      imageBox.src = fileList[currentImage]
       break;
   }
 
+    // Keep track of what image we are on 
+    
+    if (currentImage == fileList.length) {
+      currentImage = 0
+    }
+
+    // Set image 
+    imageBox.src = fileList[currentImage]
+    imageCount.innerText = `${ currentImage+1 } \\ ${ fileList.length }`
 }
